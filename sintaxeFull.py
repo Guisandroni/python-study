7c587933df65c92480560010a45076978b5e1616
guisandroni <gui.sandroni@gmail.com>
2025-07-02 07:32:42 -0300

    add initial implementation of sintaxeFull.py and create loops.py file

---
+++ sintaxeFull.py
@@ -0,0 +1,40 @@
+nome = 'guilherme'
+sobrenome = 'sandroni'
+ultimo_nome = 'dias'
+idade = 24
+endereco = 'ibiuna sp'
+
+print( f'O usuario chamado {nome} {sobrenome} {ultimo_nome} Mora em {endereco} e tem {idade} anos.' )
+
+
+preco =' 10.5'
+desconto = '0.2'
+
+print (type(preco))
+print (float(desconto))
+
+#usuario = input ( ' qual seu nome usuaro?')
+
+#print (usuario)
+
+print ( 1 + 1 , 2 - 3 , 2 * 2 , 10 / 2 , 10 // 3 , 10 % 2)
+a = 10
+b = 3
+print (a > b, a < b, a >= b, a <= b, a == b, a != b)
+
+#and e or operadores logicos
+
+print ( a >= b and a!= b , a >= b or a!= b)
+
+nomeNovo = nome
+# comparacao de variaveis em mesma regiao de memoria
+nomeNovo is nome
+
+print ('associacao arrays')
+feira = ['banana', 'maça', 'laranja', 'uva']
+
+print('banana' in feira)
+
+print('ameixa' in feira)
+x = (22-10) * 3
+print ( x )
